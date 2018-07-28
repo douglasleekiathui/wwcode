339ae151cd8309d525e3ff12575d7c4e0f26e83a
KohMengGuan <kohmengguan@hotmail.com>
2018-07-28 14:36:26 +0800

    mengguan Applicant details

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ export default new Router({
       }
     },
     {
-      path: '/jobs/applicants',
+      path: '/jobs/:id',
       components: {
         default:Applicants,
         layout:CompanyLayout
