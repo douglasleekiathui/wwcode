bcb9f1ca0e8008252602662270797fe9b5f019f8
SiewLinYap <siewlin2009@gmail.com>
2018-07-28 15:18:00 +0800

    seeker details update

--- src/components/seekers/Details.vue
+++ src/components/seekers/Details.vue
@@ -8,14 +8,15 @@
 
         <v-card-title primary-title>
           <div>
+            <i><small>Applicant ID: 201807283716</small></i>
             <h3 class="headline mb-0"><b>Portfolio Projects</b></h3>
             <div>
                 <ul style="list-style-type:square">
-                <li> <a src="http://chriscoger.com/#portfolioModal-1">Blocks</a> : An intuitive reporting platform used by political campaigns and issue advocacy organizations. Blocks organizes all of the program's data in one place; Data is synchronized dynamically so that people can spend less time connecting to data and more time producing critical insights about the program.</li>
+                <li> <a href="">Blocks</a> : An intuitive reporting platform used by political campaigns and issue advocacy organizations. Blocks organizes all of the program's data in one place; Data is synchronized dynamically so that people can spend less time connecting to data and more time producing critical insights about the program.</li>
                 <br/>
-                <li> <a src="http://chriscoger.com/#portfolioModal-2">Downbeat</a> : A platform for artists to share their music with their fans. The web application allows artists to grow their audience through social media engagement. Users follow the artist on Twitter, SoundCloud, or Youtube, and in exchange they receive access to song downloads.</li>
+                <li> <a href="">Downbeat</a> : A platform for artists to share their music with their fans. The web application allows artists to grow their audience through social media engagement. Users follow the artist on Twitter, SoundCloud, or Youtube, and in exchange they receive access to song downloads.</li>
                 <br/>
-                <li> <a src="http://chriscoger.com/#portfolioModal-4">Polling Location Lookup Tool</a> : A responsively designed web app allowing voters to find the closest polling location to their current location or address. This tool uses geolocation and GIS to empower voters to locate the easiest site for them to use to cast their vote. This product was developed with Nevada in mind, and accommodates Nevada's unique early voting system. Even though early voting locations vary by day, the Polling Location Lookup always allows voters to find the closest open polling location to them each day. The functions of this tool are modular, and can operate as a stand alone site or they can be embedded in existing sites or resources. This tool was used in Nevada by President Barack Obama's Re-Election Campaign in 2012.</li>
+                <li> <a href="">Polling Location Lookup Tool</a> : A responsively designed web app allowing voters to find the closest polling location to their current location or address. This tool uses geolocation and GIS to empower voters to locate the easiest site for them to use to cast their vote. This product was developed with Nevada in mind, and accommodates Nevada's unique early voting system. Even though early voting locations vary by day, the Polling Location Lookup always allows voters to find the closest open polling location to them each day. The functions of this tool are modular, and can operate as a stand alone site or they can be embedded in existing sites or resources. This tool was used in Nevada by President Barack Obama's Re-Election Campaign in 2012.</li>
                 <br/>
                 </ul>
             </div>
@@ -24,14 +25,14 @@
             <div>
                 <div> <b>Individual</b></div>
                 <ul style="list-style-type:square">
-                <li> Hacker Rank : <a src="https://www.hackerrank.com/contests/openbracket-2017">OpenBracket Delaware</a> Ranks# 119 / 2811 </li>
-                <li> Hacker Rank : <a src="">CodeAgon</a> Rank# 421 / 1837 </li>
+                <li> Hacker Rank : <a href="https://www.hackerrank.com/contests/openbracket-2017">OpenBracket Delaware</a> Top 120 (2811 contestants) </li>
+                <li> Hacker Rank : <a href="">CodeAgon</a> Top100 (1837 contestants) </li>
                 </ul>
 
                 <div> <b>Team</b></div>
                 <ul style="list-style-type:square">
-                <li> Kaggle : <a src="https://www.kaggle.com/c/avito-demand-prediction/leaderboard">Avito Demand Prediction Challenge </a> Rank# 22 / 1917 </li>
-                <li> Kaggle : <a src="https://www.kaggle.com/c/talkingdata-adtracking-fraud-detection"> TalkingData AdTracking Fraud Detection Challenge </a> Ranks# 166 / 3967 </li>
+                <li> Kaggle : <a href="https://www.kaggle.com/c/avito-demand-prediction/leaderboard">Avito Demand Prediction Challenge </a> Top25 / 1917 </li>
+                <li> Kaggle : <a href="https://www.kaggle.com/c/talkingdata-adtracking-fraud-detection"> TalkingData AdTracking Fraud Detection Challenge </a> Top180 / 3967 </li>
                 </ul>
                 <br/>
             </div>
@@ -39,8 +40,8 @@
             <h3 class="headline mb-0"><b>Voluntary Works</b></h3>
             <div>
                 <ul style="list-style-type:square">
-                <li> <a src="https://www.womenwhocode.com/singapore">Women Who Code Singapore</a> : Event & Meet-Ups coordinator</li>
-                <li> <a src="https://www.datakind.org/chapters/datakind-sg">DataKind SG</a> : Data Cleaning & Exploratory Analysis</li>
+                <li> <a href="https://www.womenwhocode.com/singapore">Women Who Code Singapore</a> : Event & Meet-Ups coordinator</li>
+                <li> <a href="https://www.datakind.org/chapters/datakind-sg">DataKind SG</a> : Data Cleaning & Exploratory Analysis</li>
                 <br/>
                 </ul>
             </div>
